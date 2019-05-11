1cc5f8c842f8827a6fba5031c4c5af7054a14d11
Vincent Bii <biivincent4@gmail.com>
2019-05-11 12:18:09 +0300

    First Edition

--- gs/settings.py
+++ gs/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-# import django_heroku
+import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -134,4 +134,4 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
