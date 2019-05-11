94a61111c539b95d0894ee4258fad759b7e390ec
Vincent Bii <biivincent4@gmail.com>
2019-05-11 12:54:58 +0300

    Releases:

--- gs/settings.py
+++ gs/settings.py
@@ -46,7 +46,11 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_PERMISSION_CLASSES':('rest_framework.permissions.DjangoModelPermissions'),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
 
 MIDDLEWARE = [
--- gs/urls.py
+++ gs/urls.py
@@ -4,6 +4,11 @@ from django.contrib import admin
 from rest_framework import routers
 from administration import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
@@ -25,5 +30,7 @@ urlpatterns = [
     # path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/token/$', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    url(r'^api/token/refresh/$', TokenRefreshView.as_view(), name='token_refresh'),
 ]
